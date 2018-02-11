34513e222e6953213dbbe2a2ca7db1868803a9ff
BassYang <yjdaio@163.com>
2018-02-11 08:52:39 +0800

    发布线上环境契约存储过程
    Task_AutoActFenHongContract01_15: 处理1号-15号契约分红
    Act_FenHongContract01_15: 处理1号-15号契约分红
    Act_UserAgentFHOperTran: 添加账户分户记录
    
    Task_AutoActFenHongContract16_31: 处理16号-31号契约分红
    Act_FenHongContract16_31: 处理16号-31号契约分红
    Act_UserAgentFHOperTran: 添加账户分红记录
    
    Task_AutoActGongzi_Contract: 处理日结工资
    Act_Gongzi_Contract: 处理日结工资
    Act_ContractGZOperTran: 添加账户工资记录
    
    Task_AutoActFenHong01_15_Group3
    Task_AutoActFenHong01_15_Group4
    Task_AutoActFenHong16_31_Group3
    Task_AutoActFenHong16_31_Group4
    		处理用户组默认系统分红契约
    
    Task_AutoActGongZi_Group2, Task_AutoActGongZi_Group3, Task_AutoActGongZi_Group4
    		处理用户组默认系统公资契约

---
+++ document/Contract/契约统计活动配置debug.sql
@@ -0,0 +1,14 @@
+
+SELECT * FROM N_UserContractDetail;
+
+--定时任务(活动)
+SELECT * FROM Act_ActiveSet;
+
+--系统契约
+SELECT * FROM Act_Day15FHSet;
+
+--消费记录表
+SELECT * FROM N_UserMoneyStatAll;
+
+--统计用户Id
+SELECT UserCode, * FROM N_User;
